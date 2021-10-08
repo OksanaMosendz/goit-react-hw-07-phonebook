723940abc7170f490357f3a1bb92ceda2a4d4fa5
OksanaMosendz <bagheera03@gmail.com>
2021-10-09 01:32:24 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,9 @@
 import { contactsReducer } from './phonebook/phonebook-reducers';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'items',
-  storage,
-  blacklist: ['filter'],
-};
-
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
+import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(persistConfig, contactsReducer),
+    contacts: contactsReducer,
   },
-  middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
-
-export const persistor = persistStore(store);
